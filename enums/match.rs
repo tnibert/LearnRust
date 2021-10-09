88c9986a3aade565a7c35841aa829e84dd3bba33
Tim Nibert <docz2a@gmail.com>
2021-10-09 14:02:07 +1100

    match and if let

--- enums/match.rs
+++ enums/match.rs
@@ -17,7 +17,7 @@ fn value_in_cents(coin: Coin) -> u8 {
         Coin::Penny => 1,
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        // thhis will never run
+        // this will never run
         Coin::Penny => {
             println!("jackpot");
             100
@@ -31,6 +31,29 @@ fn value_in_cents(coin: Coin) -> u8 {
 }
 
 fn main() {
+    // can define function inside function
+    // match for Option
+    fn plus_one(x: Option<i32>) -> Option<i32> {
+        match x {
+            None => None,
+            Some(i) => Some(i + 1),
+        }
+    }
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    // the _ placeholder
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (),
+    }
+
     // let amount = value_in_cents(Coin::Penny);
     let amount = value_in_cents(Coin::Quarter(UsState::Alaska));
     println!("amount: {}", amount);

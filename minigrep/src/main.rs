b30ef5f30da5e6e893b9fbbcfd8510cb91ddff68
Tim Nibert <docz2a@gmail.com>
2021-10-30 20:52:59 +1100

    read in file

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     // collect() turns an iterator into a collection
@@ -11,4 +12,9 @@ fn main() {
 
     println!("Searching for {}", query);
     println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
